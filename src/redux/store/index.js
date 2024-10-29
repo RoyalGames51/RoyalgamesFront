20dc28d57b7dfb694fe811ebc8985300d4ba39ad
JoseBrav <josebravo2015@gmail.com>
2024-10-29 20:39:26 -0300

    thunk2

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,6 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from "../reducer/index";
- import thunk from 'redux-thunk'
+ import { thunk } from 'redux-thunk';
  import {withExtraArgument} from 'redux-thunk';
 //  import choferes from '../../utils/chofer';
 
