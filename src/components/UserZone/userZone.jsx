4c0d8b045767b4c010f91403b964b8642c57163e
JoseBrav <josebravo2015@gmail.com>
2024-11-02 22:06:27 -0300

    avatar

--- src/components/UserZone/userZone.jsx
+++ src/components/UserZone/userZone.jsx
@@ -1,18 +1,19 @@
-import { Box, Avatar, Text, Stack } from "@chakra-ui/react";
+import { Box, Avatar, Text, Stack, Flex } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 
 export default function UserZone() {
   const { currentUser } = useSelector((state) => state);
 
   return (
-    <Box>
+    <Flex bgColor={"blue"} w={"10%"} h={"5%"}>
       {currentUser?.id ? (
         <Stack align="center">
           <Avatar src={currentUser.avatar} /> {/* Agrega el src al avatar del usuario */}
-          <Text fontWeight="bold">{currentUser.nickname}</Text> {/* Nickname */}
+          <Text fontWeight="bold">{currentUser.nick}</Text> {/* Nickname */}
           <Text color="gray.500">{`Chips: ${currentUser.chips}`}</Text> {/* Fichas */}
         </Stack>
       ) : null}
-    </Box>
+    </Flex>
   );
 }
+ 
