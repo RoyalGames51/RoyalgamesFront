a0b7c477e11ca96ed02ce69fa3b6cf8be1618dea
JoseBrav <josebravo2015@gmail.com>
2024-11-21 23:07:10 -0300

    loginform

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,20 +1,6 @@
 import { useState } from "react";
-import {
-    Container,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    Button,
-    InputRightElement,
-    Stack,
-    Text,
-    Box,
-    Center,
-    IconButton,
-    Image,
-} from "@chakra-ui/react";
-import { ViewIcon, ViewOffIcon, CloseIcon } from "@chakra-ui/icons";
+import { Box, Button, IconButton, Input, InputGroup, InputRightElement, Stack, Text, FormControl, FormLabel, Image, Center, Flex } from "@chakra-ui/react";
+import { CloseIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import googleLogo from "../../assets/IMG_3867.png";
 import { useAuth } from "../../context/oauthContext";
 import { useDispatch, useSelector } from "react-redux";
@@ -138,135 +124,156 @@ export default function Login() {
 
     return (
         <Box>
-            {/* Botón para abrir el cuadro de inicio de sesión */}
-            <Button
-                onClick={toggleLoginBox}
-                bgGradient="linear(to-r, #1a880f, #8eff17)" // Gradiente de color
-                color="white"
-                size="lg"
-                fontSize="lg"
-                fontWeight="bold"
-                borderRadius="full"
-                boxShadow="0 4px 6px rgba(0, 0, 0, 0.1)" // Sombra suave
-                _hover={{
-                    bgGradient: "linear(to-r, #8eff17, #1a880f)", // Cambia el gradiente en hover
-                    boxShadow: "0 6px 10px rgba(0, 0, 0, 0.2)", // Sombra más fuerte en hover
-                    transform: "scale(1.05)", // Leve efecto de agrandamiento en hover
-                }}
-                _active={{
-                    bgGradient: "linear(to-r, teal.500, blue.600)", // Color al hacer clic
-                    transform: "scale(0.95)", // Ligeramente más pequeño al hacer clic
-                }}
-                _focus={{
-                    outline: "none",
-                    boxShadow: "0 0 1px 2px teal.500", // Resaltado en el foco
-                }}
+          {/* Botón para abrir el cuadro de inicio de sesión */}
+          <Button
+            onClick={toggleLoginBox}
+            bgGradient="linear(to-r, #1a880f, #8eff17)" // Gradiente de color
+            color="white"
+            size="lg"
+            fontSize="lg"
+            fontWeight="bold"
+            borderRadius="full"
+            boxShadow="0 4px 6px rgba(0, 0, 0, 0.1)" // Sombra suave
+            _hover={{
+              bgGradient: "linear(to-r, #8eff17, #1a880f)", // Cambia el gradiente en hover
+              boxShadow: "0 6px 10px rgba(0, 0, 0, 0.2)", // Sombra más fuerte en hover
+              transform: "scale(1.05)", // Leve efecto de agrandamiento en hover
+            }}
+            _active={{
+              bgGradient: "linear(to-r, teal.500, blue.600)", // Color al hacer clic
+              transform: "scale(0.95)", // Ligeramente más pequeño al hacer clic
+            }}
+            _focus={{
+              outline: "none",
+              boxShadow: "0 0 1px 2px teal.500", // Resaltado en el foco
+            }}
+          >
+            Iniciar sesión
+          </Button>
+    
+          {/* Fondo oscuro y cuadro de inicio de sesión */}
+          {isLoginOpen && (
+            <Box
+              position="fixed"
+              top="0"
+              left="0"
+              right="0"
+              bottom="0"
+              bg="rgba(0, 0, 0, 0.6)"
+              zIndex="1000"
+              onClick={toggleLoginBox} // Cierra al hacer clic en el fondo oscuro
             >
-                Iniciar sesión
-            </Button>
-
-            {/* Fondo oscuro y cuadro de inicio de sesión */}
-            {isLoginOpen && (
-                <Box
-                    position="fixed"
-                    top="0"
-                    left="0"
-                    right="0"
-                    bottom="0"
-                    bg="rgba(0, 0, 0, 0.6)"
-                    zIndex="1000"
-                    onClick={toggleLoginBox} // Cierra al hacer clic en el fondo oscuro
-                >
-                    <Center h="100vh">
+              <Flex
+                        position="fixed"
+                        top="50%"
+                        left="50%"
+                        transform="translate(-50%, -50%)"
+                        bg="white"
+                        
+                        boxShadow="2xl"
+                        borderRadius="15px"
+                        
+                        zIndex="10"
+                        align="center"
+                        direction={"column"}
+                    >
                         <Box
-                            w="100%"
-                            maxW="400px"
-                            bg="white"
-                            p={8}
-                            boxShadow="lg"
-                            borderRadius="lg"
-                            onClick={(e) => e.stopPropagation()} // Evita que el clic cierre el cuadro
-                        >
-                            <Stack spacing={4}>
-                                {/* Botón de cierre (X) */}
-                                <Box display="flex" justifyContent="flex-end">
-                                    <IconButton
-                                        aria-label="Cerrar"
-                                        icon={<CloseIcon />}
-                                        size="sm"
-                                        onClick={toggleLoginBox}
-                                        bg="transparent"
-                                        _hover={{ bg: "red.200" }}
-                                    />
-                                </Box>
-
-                                <FormControl>
-                                    <FormLabel fontSize="lg" color="black">Correo Electrónico</FormLabel>
-                                    <Input
-                                        bg="white"
-                                        type="text"
-                                        value={input.email}
-                                        onChange={handleInputChange}
-                                        placeholder="Ingresa tu Nick o Email"
-                                        name="email"
-                                        fontSize="md"
-                                        color="black"
-                                    />
-                                </FormControl>
-
-                                <FormControl isRequired>
-                                    <FormLabel fontSize="lg" color="black">Contraseña</FormLabel>
-                                    <InputGroup size="md">
-                                        <Input
-                                            bg="white"
-                                            type={show ? "text" : "password"}
-                                            placeholder="Ingresa tu contraseña"
-                                            name="password"
-                                            onChange={handleInputChange}
-                                            fontSize="md"
-                                            color="black"
-                                        />
-                                        <InputRightElement width="4rem">
-                                            <Button
-                                                h="1rem"
-                                                size="lg"
-                                                onClick={handleClick}
-                                                bg="transparent"
-                                                _hover={{ bg: "transparent" }}
-                                                _active={{ bg: "transparent" }}
-                                            >
-                                                {show ? <ViewOffIcon /> : <ViewIcon />}
-                                            </Button>
-                                        </InputRightElement>
-                                    </InputGroup>
-                                </FormControl>
-
-                                <Button
-                                    m={2}
-                                    p={5}
-                                    bg="#E8D980"
-                                    onClick={handleSubmit}
-                                    w="100%" // Ancho completo del botón
-                                >
-                                    Entrar
-                                </Button>
-
-                                <Button
-                                    bg="white"
-                                    fontFamily="'DIN Medium',"
-                                    color="black"
-                                    onClick={handleGoogleLogin}
-                                    leftIcon={<Image src={googleLogo} alt="Google Logo" boxSize="25px" />}
-                                    w="100%"
-                                >
-                                    Continuar con Google
-                                </Button>
-
-                            </Stack>
-                        </Box>
-                    </Center>
+                    bg="#1a880f"
+                    color="white"
+                    textAlign="center"
+                    p={3}
+                    borderTopRadius="lg"
+                    fontWeight="bold"
+                    fontSize="xl"
+                    w={"100%"}
+                  >
+                    Iniciar sesión
+                  </Box>
+                <Box
+                  w="100%"
+                  maxW="300px"
+                  bg="white"
+                  pb={8}
+                  pl={8}
+                  pr={8}
+                  boxShadow="lg"
+                  borderRadius="lg"
+                  onClick={(e) => e.stopPropagation()} // Evita que el clic cierre el cuadro
+                >
+                  {/* Encabezado con fondo verde y borderRadius */}
+                  
+    
+                  <Stack spacing={4} mt={4}>
+                   
+    
+                    {/* Campo de correo electrónico */}
+                    <FormControl>
+                     
+                      <Input
+                        bg="white"
+                        type="text"
+                        value={input.email}
+                        onChange={handleInputChange}
+                        placeholder="Ingresa tu Nick o Email"
+                        name="email"
+                        fontSize="md"
+                        color="black"
+                        borderColor="gray.300"
+                        _focus={{ borderColor: "#1a880f" }} // Color de borde al hacer foco
+                      />
+                    </FormControl>
+    
+                    {/* Campo de contraseña */}
+                    <FormControl isRequired>
+                      
+                      <InputGroup size="md">
+                        <Input
+                          bg="white"
+                          type={show ? "text" : "password"}
+                          placeholder="Ingresa tu contraseña"
+                          name="password"
+                          onChange={handleInputChange}
+                          fontSize="md"
+                          color="black"
+                          borderColor="gray.300"
+                          _focus={{ borderColor: "#1a880f" }}
+                        />
+                        <InputRightElement width="4rem">
+                          <Button
+                            h="1rem"
+                            size="lg"
+                            onClick={handleClick}
+                            bg="transparent"
+                            _hover={{ bg: "transparent" }}
+                            _active={{ bg: "transparent" }}
+                          >
+                            {show ? <ViewOffIcon /> : <ViewIcon />}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                    </FormControl>
+    
+                    {/* Botón de inicio de sesión */}
+                    <Button
+                      m={2}
+                      p={5}
+                      bg="#E8D980"
+                      onClick={handleSubmit}
+                      w="100%" // Ancho completo del botón
+                      _hover={{
+                        bg: "#d4c759", // Hover para cambiar el color de fondo
+                        transform: "scale(1.05)",
+                      }}
+                    >
+                      Entrar
+                    </Button>
+    
+                    {/* Aquí puedes agregar más botones o enlaces si lo deseas, como el de Google */}
+                  </Stack>
                 </Box>
-            )}
+                </Flex>
+            </Box>
+          )}
         </Box>
-    );
-}
+      );
+    };
