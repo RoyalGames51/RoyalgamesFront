c3599536b45096a3783e835168791b59d3e9b55e
JoseBrav <josebravo2015@gmail.com>
2024-12-17 19:39:20 -0300

    size

--- src/components/Juegos/Diamantes/diamantes.jsx
+++ src/components/Juegos/Diamantes/diamantes.jsx
@@ -6,31 +6,42 @@ const Diamantes = () => {
   // Obtener el jugadorID del estado global
   const currentUser = useSelector((state) => state.currentUser);
   const jugadorID = currentUser?.id || "default-id"; // Valor por defecto si no hay usuario
-  
+
   // URL del juego con el jugadorID
   const gameURL = `https://html-classic.itch.zone/html/12285943/juego8/index.html?jugadorID=${jugadorID}`;
 
   return (
     <Box
-      w="100%"
-      h="100vh"
+      w="100vw"
+      h="calc(100vh - 60px)" // Asegúrate de que esto reste exactamente la altura de la Navbar
       bg="gray.900"
       display="flex"
-      justifyContent="center"
-      alignItems="center"
+      flexDirection="column"
     >
+      {/* Contenedor del juego */}
       {jugadorID ? (
-        <iframe
-          src={gameURL}
-          title="Juego 2"
-          width="100%"
-          height="100%"
-          style={{
-            border: "none",
-          }}
-        />
+        <Box
+          flex="1" // Ocupa el espacio restante considerando la Navbar
+          position="relative"
+          w="100%"
+          bg="gray.800"
+          overflow="hidden"
+        >
+          <iframe
+            src={gameURL}
+            title="Juego Diamantes"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              border: "none",
+            }}
+          />
+        </Box>
       ) : (
-        <Center>
+        <Center h="100%">
           <Spinner size="xl" color="teal.300" />
         </Center>
       )}
