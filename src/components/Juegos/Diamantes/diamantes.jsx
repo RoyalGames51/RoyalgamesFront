bb2db38241ccecf7f321aa9e1de00e669b5742cb
JoseBrav <josebravo2015@gmail.com>
2024-12-27 21:06:09 -0300

    j26

--- src/components/Juegos/Diamantes/diamantes.jsx
+++ src/components/Juegos/Diamantes/diamantes.jsx
@@ -8,7 +8,7 @@ const Diamantes = () => {
   const jugadorID = currentUser?.id || "default-id"; // Valor por defecto si no hay usuario
 
   // URL del juego con el jugadorID
-  const gameURL = `https://html-classic.itch.zone/html/12359750/juego25/index.html?jugadorID=${jugadorID}`;
+  const gameURL = `https://html-classic.itch.zone/html/12359792/juego26/index.html?jugadorID=${jugadorID}`;
 
   return (
     <Box
